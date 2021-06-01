4fb842c54a2754a92ee9c21cd4705584f63ed83e
trainSu <948215098@qq.com>
2021-06-01 09:46:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,11 +3,11 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
-#Import external feeds
+# Import external feeds
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/app-adguardhome
 
-#agron_new
+# agron_new
 cd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
