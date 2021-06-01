83e2b792dab0017fc5d1cc0d894f07c8fd799fed
trainSu <948215098@qq.com>
2021-06-01 09:47:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,7 +6,7 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-# passwall
+# Import external feeds
 cd package/lean
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
