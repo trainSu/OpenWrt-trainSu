d01d5cd3423d403425ccd6b62bd23d16fe23d557
trainSu <948215098@qq.com>
2021-06-02 19:26:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,8 +7,8 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Import external feeds
-cd package/lean
+#cd package/lean
 #git clone https://github.com/xiaorouji/openwrt-passwall.git
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+#git clone https://github.com/jerrykuku/lua-maxminddb.git
+#git clone https://github.com/jerrykuku/luci-app-vssr.git
