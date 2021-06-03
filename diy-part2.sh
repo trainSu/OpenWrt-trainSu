f283e17e5b2910e65b387c0bbdb99a7004b78ab2
trainSu <948215098@qq.com>
2021-06-03 11:55:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: trainSu
+#=================================================
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
