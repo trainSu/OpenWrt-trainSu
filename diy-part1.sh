1d52043a5bc74fedf1e7f1217e9266bcb119035b
trainSu <948215098@qq.com>
2022-02-18 10:03:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 cd package/lean
 git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+# rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
