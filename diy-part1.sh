b28e6d2c54356c3da91f7bf06c0b750a0abb9309
trainSu <948215098@qq.com>
2022-03-09 12:55:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,6 +13,5 @@
 
 # Import external feeds
 cd package/lean
-git clone https://github.com/xiaorouji/openwrt-passwall.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
