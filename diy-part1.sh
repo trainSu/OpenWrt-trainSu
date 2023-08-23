af180dea8122ec0bc31b279c010467f73372f148
trainSu <948215098@qq.com>
2023-08-23 12:45:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,10 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Import external feeds
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 cd package/lean
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
 # git clone https://github.com/vernesong/OpenClash.git
