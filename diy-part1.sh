49b71cec03387e2031e2429ab93bf4e213c01e2a
trainSu <948215098@qq.com>
2024-12-04 22:33:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,12 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Import external feeds
+cd package/lean
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
