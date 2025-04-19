fbd3b4cebc52613e176931dd0c075800d28eddcb
trainSu <948215098@qq.com>
2025-04-19 23:43:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,9 +11,4 @@
 # Import external feeds
 mkdir package/luci-app-openclash
 cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
+git clone --depth=1 https://github.com/vernesong/OpenClash.git
