3a93fd63eb5342bfbc4e68a2d61043be17490dd3
trainSu <948215098@qq.com>
2025-04-19 23:14:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,6 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Import external feeds
-find ./ | grep Makefile | grep mosdns | xargs rm -f
 mkdir package/luci-app-openclash
 cd package/luci-app-openclash
 git init
@@ -18,5 +17,3 @@ git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
-cd
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
