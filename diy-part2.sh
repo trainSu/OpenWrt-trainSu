64d30b46a044c9616a719e06968cc859ced9bce8
trainSu <948215098@qq.com>
2026-04-11 02:28:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,4 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 精确修改核心模块的路径定义
-sed -i 's/"parent": "admin\/system\/plugins"/"parent": "admin\/system"/g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/*.json
+sed -i 's/"parent": "admin\/system\/plugins"/"parent": "admin\/system"/g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
