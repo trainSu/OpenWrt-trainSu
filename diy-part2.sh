341ff1f31731051f19d4b08f6243778d1b3c8d3f
trainSu <948215098@qq.com>
2026-04-11 04:54:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,6 +10,3 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# 消除 Plugins 菜单
-find package feeds -name "*.json" 2>/dev/null | xargs -r sed -i 's/"parent": "admin\/system\/plugins"/"parent": "admin\/system"/g'
-sed -i 's/"admin\/system\/plugins"/"admin\/system\/_plugins_hidden"/g' feeds/luci/modules/luci-mod-system/root/usr/share/luci/menu.d/luci-mod-system.json
