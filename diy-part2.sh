9bc8d85a0e5fa76553184307d2d6a2b7ebc28881
trainSu <948215098@qq.com>
2026-04-11 02:47:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,5 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 # find ./ | grep Makefile | grep mosdns | xargs rm -f
 # git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# 强制把核心系统定义的 Plugins 挪走
-find feeds/luci/modules/luci-mod-system -name "*.json" | xargs sed -i 's/admin\/system\/plugins/admin\/system/g'
+# 消除 Plugins 菜单
+find package feeds -name "*.json" | xargs -r sed -i 's/"parent": "admin\/system\/plugins"/"parent": "admin\/system"/g'
