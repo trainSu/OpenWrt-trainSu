c79298408eb9e375eb5b614f455f7545d5be9d9f
trainSu <948215098@qq.com>
2026-04-27 17:22:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,4 +9,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # Import external feeds
-# git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
