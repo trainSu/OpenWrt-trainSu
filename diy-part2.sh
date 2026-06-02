12b4c593220749695ee4c7aa44d747f709a31b38
trainSu <948215098@qq.com>
2026-06-02 17:36:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # 开启缓存
 # sed -i -E '/^(# )?CONFIG_(DEVEL|CCACHE)(=| is)/d' .config
 # echo -e "CONFIG_DEVEL=y\nCONFIG_CCACHE=y" >> .config
